1507a9538a52e61a5def7a421f222400a45501a5
Wadeck Follonier <wadeck.follonier@gmail.com>
2019-02-04 10:20:10 +0100

    Put the test case inside the base class

--- src/test/java/com/cloudbees/jenkins/plugins/sshcredentials/impl/BasicSSHUserPrivateKeyTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/sshcredentials/impl/BasicSSHUserPrivateKeyTest.java
@@ -24,6 +24,7 @@
 
 package com.cloudbees.jenkins.plugins.sshcredentials.impl;
 
+import com.cloudbees.hudson.plugins.folder.Folder;
 import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsScope;
@@ -31,14 +32,25 @@ import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import java.util.List;
 import hudson.FilePath;
+import hudson.cli.CLICommandInvoker;
+import hudson.cli.UpdateJobCommand;
 import hudson.model.Hudson;
+import hudson.model.Job;
 import hudson.remoting.Callable;
 import hudson.security.ACL;
+import jenkins.model.Jenkins;
 import jenkins.security.MasterToSlaveCallable;
 
 import org.junit.Test;
+
+import static hudson.cli.CLICommandInvoker.Matcher.failedWith;
+import static hudson.cli.CLICommandInvoker.Matcher.succeeded;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 import org.junit.Rule;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
@@ -70,4 +82,38 @@ public class BasicSSHUserPrivateKeyTest {
 
     // TODO demonstrate that all private key sources are round-tripped in XStream
 
+    @Test
+    @Issue("SECURITY-440")
+    @LocalData("updateJob")
+    public void userWithoutRunScripts_cannotMigrateDangerousPrivateKeySource() throws Exception {
+        Folder folder = r.jenkins.createProject(Folder.class, "folder1");
+        
+        FilePath updateFolder = r.jenkins.getRootPath().child("update_folder.xml");
+        
+        { // as user with just configure, you cannot migrate
+            CLICommandInvoker.Result result = new CLICommandInvoker(r, new UpdateJobCommand())
+                    .authorizedTo(Jenkins.READ, Job.READ, Job.CONFIGURE)
+                    .withStdin(updateFolder.read())
+                    .invokeWithArgs("folder1");
+            
+            assertThat(result.stderr(), containsString("user is missing the Overall/RunScripts permission"));
+            assertThat(result, failedWith(-1));
+            
+            // config file not touched
+            String configFileContent = folder.getConfigFile().asString();
+            assertThat(configFileContent, not(containsString("FileOnMasterPrivateKeySource")));
+            assertThat(configFileContent, not(containsString("BasicSSHUserPrivateKey")));
+        }
+        { // but as admin with RUN_SCRIPTS, you can
+            CLICommandInvoker.Result result = new CLICommandInvoker(r, new UpdateJobCommand())
+                    .authorizedTo(Jenkins.ADMINISTER)
+                    .withStdin(updateFolder.read())
+                    .invokeWithArgs("folder1");
+            
+            assertThat(result, succeeded());
+            String configFileContent = folder.getConfigFile().asString();
+            assertThat(configFileContent, containsString("BasicSSHUserPrivateKey"));
+            assertThat(configFileContent, not(containsString("FileOnMasterPrivateKeySource")));
+        }
+    }
 }
