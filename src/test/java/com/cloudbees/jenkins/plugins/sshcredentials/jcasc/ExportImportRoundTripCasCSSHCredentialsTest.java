072a8fc2d8ae7a65465e62c65a50efffda70ee4b
Ramon Leon <manuelramonleonjimenez@gmail.com>
2019-06-03 18:28:43 +0200

    [JENKINS-57824] Add full JCasC test

---
+++ src/test/java/com/cloudbees/jenkins/plugins/sshcredentials/jcasc/ExportImportRoundTripCasCSSHCredentialsTest.java
@@ -0,0 +1,48 @@
+package com.cloudbees.jenkins.plugins.sshcredentials.jcasc;
+
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import io.jenkins.plugins.casc.misc.ExportImportRoundTripAbstractTest;
+import jenkins.model.Jenkins;
+import org.hamcrest.Matchers;
+import org.jvnet.hudson.test.RestartableJenkinsRule;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class ExportImportRoundTripCasCSSHCredentialsTest extends ExportImportRoundTripAbstractTest {
+    @Override
+    public void assertConfiguredAsExpected(RestartableJenkinsRule restartableJenkinsRule, String s) {
+        List<StandardUsernamePasswordCredentials> creds = CredentialsProvider.lookupCredentials(StandardUsernamePasswordCredentials.class, Jenkins.getInstanceOrNull(), null, Collections.emptyList());
+        assertEquals(1, creds.size());
+        StandardUsernamePasswordCredentials cred = creds.get(0);
+        assertEquals("userid", cred.getId());
+        assertEquals("username-of-userid", cred.getUsername());
+        assertEquals("password-of-userid", cred.getPassword().getPlainText());
+
+        List<BasicSSHUserPrivateKey> creds2 = CredentialsProvider.lookupCredentials(BasicSSHUserPrivateKey.class,Jenkins.getInstanceOrNull(), null, Collections.emptyList());
+        assertEquals(1, creds2.size());
+        BasicSSHUserPrivateKey cred2 = creds2.get(0);
+        assertEquals("userid2", cred2.getId());
+        assertEquals("username-of-userid2", cred2.getUsername());
+        assertEquals("passphrase-of-userid2", cred2.getPassphrase().getPlainText());
+        assertEquals("the description of userid2", cred2.getDescription());
+        assertEquals(1, cred2.getPrivateKeySource().getPrivateKeys().size());
+        String directKey = cred2.getPrivateKeySource().getPrivateKeys().get(0);
+        assertThat(directKey, Matchers.containsString("Piikjvd/2HYfEYEG"));
+    }
+
+    @Override
+    public String configResource() {
+        return "ssh-credentials.yml";
+    }
+
+    @Override
+    public String stringInLogExpected() {
+        return "passphrase = passphrase-of-userid2";
+    }
+}
