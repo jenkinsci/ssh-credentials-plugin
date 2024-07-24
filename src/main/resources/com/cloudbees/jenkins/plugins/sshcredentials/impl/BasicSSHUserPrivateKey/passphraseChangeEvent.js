fd99628552efdd97253c4a6fd8d48d504ec2d69d
Pedro Bueno <pbuenoyerbes@cloudbees.com>
2024-07-24 16:05:00 +0200

    Using class name instead of ids

--- src/main/resources/com/cloudbees/jenkins/plugins/sshcredentials/impl/BasicSSHUserPrivateKey/passphraseChangeEvent.js
+++ src/main/resources/com/cloudbees/jenkins/plugins/sshcredentials/impl/BasicSSHUserPrivateKey/passphraseChangeEvent.js
@@ -1,8 +1,13 @@
 //TODO: this snippet, as well as ids in passphrase and private key fields can be removed once https://issues.jenkins.io/browse/JENKINS-65616 is completed
-var passphraseElement = document.getElementById('sshCredentials_passphrase');
-var privateKeyElement = document.getElementById('sshCredentials_privateKey');
+const passphraseElements = document.getElementsByClassName('sshCredentials_passphrase');
 
-passphraseElement.addEventListener("change", event => {
-    var newEvent = new Event("change")
-    privateKeyElement.dispatchEvent(newEvent)
-})
+if (passphraseElements.length > 0) {
+// Failsafe in case there's more than 1 element we'll only use the first one. Should not happen.
+    passphraseElements[0].addEventListener("change", event => {
+        var newEvent = new Event("change")
+        const privateKeyElements = document.getElementsByClassName('sshCredentials_privateKey');
+        if (passphraseElements.length > 0) {
+            privateKeyElements[0].dispatchEvent(newEvent)
+        }
+    })
+}
